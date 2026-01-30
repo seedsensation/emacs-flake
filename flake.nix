6b7a70a035b76b0eb51ad567d875b2344f456194
Mercury <m@rcury.com>
2026-01-30 12:54:31 +0000

    Updated flake to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Emacs Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
