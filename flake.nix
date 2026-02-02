bc48fb6746a70fe37e6beee5a256ad8a7a94c3de
Mercury <m@rcury.com>
2026-02-02 13:51:30 +0000

    Changed nixpkgs channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Emacs Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }:
