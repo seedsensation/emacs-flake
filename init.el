4ba3282bd3434a36433c66cf30fa604de195b615
Mercury <m@rcury.com>
2026-02-12 12:41:28 +0000

    Added easier testing

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
 ;; -*- lexical-binding: t; -*-
 
+(setq file-to-open (if (string-equal (getenv "TEST") "1") "readme-live.org" "readme.org"))
 (let ((file-name-handler-alist nil))
   ;; If config is pre-compiled, then load that.
   (if (file-exists-p (expand-file-name "readme.el" user-emacs-directory))
       (load-file (expand-file-name "readme.el" user-emacs-directory))
     ;; otherwise tangle and load
-    (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory))))
+    (org-babel-load-file (expand-file-name 'file-to-open user-emacs-directory))))
