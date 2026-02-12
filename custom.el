0bddb02775afa4116b39cc656589a01bee9f5008
Mercury <m@rcury.com>
2026-02-12 12:14:20 +0000

    Added define-keys macro

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(corfu-auto t)
  '(format-all-default-formatters
    '(("Assembly" asmfmt) ("ATS" atsfmt) ("Bazel" buildifier)
      ("BibTeX" emacs-bibtex) ("C" clang-format) ("C#" csharpier)
@@ -39,6 +40,7 @@
      ("_Flow" prettier) ("_Gleam" gleam) ("_Ledger" ledger-mode)
      ("_Nginx" nginxfmt) ("_Snakemake" snakefmt)))
  '(format-all-formatters '(("Java" astyle)) t)
+ '(global-corfu-mode t)
  '(initial-buffer-choice "~/org/contents.org")
  '(lsp-java-vmargs
    '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4"
