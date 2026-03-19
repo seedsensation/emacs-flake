f6fa4d331e8cd3364530002b8003a18ded5ceece
Mercury <m@rcury.com>
2026-03-19 15:55:31 +0000

    Updated C formatting

--- custom.el
+++ custom.el
@@ -4,6 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-default-style
+   '((c++-mode . "default") (java-mode . "java") (awk-mode . "awk")
+     (other . "gnu")))
  '(corfu-auto t)
  '(format-all-default-formatters
    '(("Assembly" asmfmt) ("ATS" atsfmt) ("Bazel" buildifier)
@@ -39,13 +42,14 @@
      ("_Beancount" bean-format) ("_Caddyfile" caddy-fmt)
      ("_Flow" prettier) ("_Gleam" gleam) ("_Ledger" ledger-mode)
      ("_Nginx" nginxfmt) ("_Snakemake" snakefmt)))
- '(format-all-formatters '(("Java" astyle)) t)
+ '(format-all-formatters '(("Java" astyle) ("Rust" rustfmt)) t)
  '(global-corfu-mode t)
  '(initial-buffer-choice "~/org/contents.org")
  '(lsp-java-vmargs
    '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4"
      "-XX:AdaptiveSizePolicyWeight=90"
      "-Dsun.zip.disableMemoryMapping=true" "-Xmx4G" "-Xms100m"))
+ '(org-confirm-babel-evaluate nil)
  '(org-enforce-todo-dependencies t)
  '(org-format-latex-options
    '(:foreground default :background default :scale 2.25 :html-foreground
@@ -54,7 +58,10 @@
  '(org-roam-directory "/home/mercury/org/")
  '(org-roam-extract-new-file-path "nodes/${slug}.org")
  '(recentf-mode t)
- '(safe-local-variable-values '((lexical-bindings . t)))
+ '(safe-local-variable-values
+   '((eval progn (org-babel-goto-named-src-block "startup")
+	   (org-babel-execute-src-block))
+     (lexical-bindings . t)))
  '(truncate-lines t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
